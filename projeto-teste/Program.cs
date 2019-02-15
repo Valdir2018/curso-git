ef44104c84d17c2d1c9c5ca2f1b5a30b44cd389d
Valdir Silva <valdirpiresba@gmail.com>
2019-02-15 14:38:51 -0200

    projeto iniciado ola mundo

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hóje é quinta feira !");
+            Console.WriteLine();
+
+
+            Console.WriteLine("Obrigado por mais um dia de vida ! obrigado, obrigado !!");
+        }
+    }
+}
